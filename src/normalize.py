9e595bc6cb3fedd7fb8173cd3969ee7110eca936
Patrik Németh <patriknemethpn@gmail.com>
2020-03-23 20:15:50 +0100

    Optimize the normalization algorithms using numpy functions
    Most of the calculations were re-written with numpy functions,
    significantly boosting normalization calculation speeds, especially with large images.

--- src/normalize.py
+++ src/normalize.py
@@ -2,38 +2,36 @@ import numpy as np
 from PIL import Image
 
 def normalizeMinMax(imgArray):
+    """Simple image normalization based on value averaging"""
     # Image normalization (as per source: Fingerprint Authentication using Gabor Filter based Matching Algorithm)
     # Get the original shape of the image and then flatten it to a 1D array
     imgArray = imgArray.flatten()
 
-    gMin = min(imgArray)
-    gMax = max(imgArray)
+    gMin = np.amin(imgArray)
+    gMax = np.amax(imgArray)
     gRange = gMax - gMin
 
     # Normalize
-    for i, v in enumerate(imgArray):
-        imgArray[i] = ((v - gMin) * 255) / gRange
-    
-    return imgArray
+    imgArray = (((np.float32(imgArray) - gMin) * 255) / gRange)
+
+    return np.uint8(imgArray)
 
 def normalizeMeanVariance(imgArray):
+    """Complex image normalization based on image mean and variance values"""
     imgArray = imgArray.flatten()
 
     MEAN = imgArray.sum() / imgArray.size
 
-    varSum = 0
-    for i in imgArray:
-        varSum = varSum + (i - MEAN) ** 2
+    varSum = ((imgArray - MEAN)**2).sum()
 
     VAR = varSum / imgArray.size
 
     DESIRED_VAR = 100
     DESIRED_MEAN = 100
 
-    for i, _ in enumerate(imgArray):
-        if imgArray[i] > MEAN:
-            imgArray[i] = DESIRED_MEAN + ((DESIRED_VAR * ((imgArray[i] - MEAN)**2)) / VAR)**(1/2.0)
-        else:
-            imgArray[i] = DESIRED_MEAN - ((DESIRED_VAR * ((imgArray[i] - MEAN)**2)) / VAR)**(1/2.0)
+    imgArray = np.float32(imgArray)
+    imgArray = np.where(imgArray > MEAN,
+                        DESIRED_MEAN + ((DESIRED_VAR * ((imgArray - MEAN)**2)) / VAR)**(1/2.0),
+                        DESIRED_MEAN - ((DESIRED_VAR * ((imgArray - MEAN)**2)) / VAR)**(1/2.0))
 
-    return imgArray
+    return np.uint8(imgArray)
