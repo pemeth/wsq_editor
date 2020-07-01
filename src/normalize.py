acaa4303f5889a2a0f91a89920333e8c78b3a7c6
Patrik Németh <patriknemethpn@gmail.com>
2020-07-01 14:44:42 +0200

    Refactor `normalizeMeanVariance`

--- src/normalize.py
+++ src/normalize.py
@@ -1,19 +1,28 @@
 import numpy as np
 
-def normalizeMeanVariance(imgArray):
-    """Complex image normalization based on image mean and variance values"""
-    MEAN = np.mean(imgArray)
-    VAR = np.var(imgArray)
+def normalizeMeanVariance(im):
+    """Image normalization based on desired image mean and variance (both 100) values.
+    
+    Parameters
+    ----------
+    im : numpy_array
+        The input grayscale image to be normalized.
+        
+    Returns
+    -------
+        Normalised image of the same size as `im`."""
+    MEAN = np.mean(im)
+    VAR = np.var(im)
 
     DESIRED_VAR = 100
     DESIRED_MEAN = 100
 
-    imgArray = np.float32(imgArray)
-    imgArray = np.where(imgArray > MEAN,
-                        DESIRED_MEAN + ((DESIRED_VAR * ((imgArray - MEAN)**2)) / VAR)**(1/2.0),
-                        DESIRED_MEAN - ((DESIRED_VAR * ((imgArray - MEAN)**2)) / VAR)**(1/2.0))
+    im = np.float32(im)
+    im = np.where(im > MEAN,
+                        DESIRED_MEAN + ((DESIRED_VAR * ((im - MEAN)**2)) / VAR)**(1/2.0),
+                        DESIRED_MEAN - ((DESIRED_VAR * ((im - MEAN)**2)) / VAR)**(1/2.0))
 
-    imMin = np.amin(imgArray)
-    imMax = np.amax(imgArray)
+    imMin = np.amin(im)
+    imMax = np.amax(im)
 
-    return np.uint8((imgArray - imMin) * (255 / (imMax - imMin)))
+    return np.uint8((im - imMin) * (255 / (imMax - imMin)))
