db1c72909c600f4f3a56b2ba39a221c267eb5d8c
Patrik Németh <patriknemethpn@gmail.com>
2020-07-19 20:58:48 +0200

    Expand docstrings

--- src/thinning.py
+++ src/thinning.py
@@ -1,14 +1,39 @@
 import numpy as np
 
 def neighborCount(window):
-    """Find the number of neighboring 1 pixels around the center pixel in a 3x3 window `window`."""
+    """Find the number of neighboring 1 pixels around the center pixel in a 3x3 window `window`.
+    
+    Parameters
+    ----------
+    window : numpy_array
+        A 3x3 window centered around a pixel for which to count neighbors.
+
+    Returns
+    -------
+        The number of neighbors around the center pixel."""
     counter = np.ones((3,3))
     counter[1,1] = 0
 
     return np.sum(window * counter)
 
 def zeroToOnePatternCount(unrolled_window):
-    """Find the number of 0 to 1 transitions in the cyclic array `unrolled_window`."""
+    """Find the number of 0 to 1 transitions in the cyclic array `unrolled_window`.
+    
+    Parameters
+    ----------
+    unrolled_window : numpy_array
+        A 1D numpy array of binary values found in a 3x3 window around a center pixel unrolled
+        in a circular fashion. This window is made cyclic for the calculations.
+        Ex.:
+            if the 3x3 window looks like this:  0 1 2
+                                                3 4 5
+                                                6 7 8,
+            then the unrolled window is the values: 0,1,2,5,8,7,6,3. Internally a ninth value is added,
+            so the window is cyclic - this value would in this case be 0.
+
+    Returns
+    -------
+        The number of 0->1 patterns in the binary `unrolled_window`."""
     # check where values change from 1 to 0 and from 0 to 1
     #   diff outputs -1 where 1->0 and 1 where 0->1
     diffs = np.diff(unrolled_window, append=unrolled_window[0])
